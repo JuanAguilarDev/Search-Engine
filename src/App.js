5c0ac5880292a42e25c91d50c8e051f2121ac507
juanaguilar <Juan_aguilar1290@outlook.es>
2022-04-20 18:00:24 -0500

    Update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import Search from './views/Search';
-import routes from './routes';
 
 
 function App() {
--- src/views/Search/index.js
+++ src/views/Search/index.js
@@ -1,19 +1,34 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import SearchBox from './components/SearchBox';
-import data from '../../data/users.json';
 import './style.css';
 import SearchResults from './components/SearchResults';
+import axios from 'axios';
 
 export default function Search() {
     // State vars
     const [isAtTop, setIsAtTop] = useState(false);
-    const [userData, setUserData] = useState(data);
+    const [userData, setUserData] = useState([]);
     const [results, setResults] = useState([]);
 
     const handleClose = () => {
         setIsAtTop(false);
         setResults([]);
     };
+
+    // Get data from an API with axios
+    useEffect(() => {
+        const getUsers = async () => {
+            try {
+                const response = await axios.get("https://jsonplaceholder.typicode.com/users");
+                setUserData(response.data);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+
+        // Call the function
+        getUsers().catch(null);
+    }, []);
 
     const handleSearchClick = (searchText) => {
         setIsAtTop(true);
